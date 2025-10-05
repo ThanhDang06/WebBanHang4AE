c9af7a9cce9e96ff8441d78c13211d768d8927b6
maith <maith@THANHDANG>
2025-10-05 23:51:30 +0700

    Add project files.

---
+++ WBH/App_Start/RouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WBH
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+
+            );
+            // Route bắt tất cả -> về Home/Index
+            routes.MapRoute(
+                name: "CatchAll",
+                url: "{*url}",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+        }
+    }
+}
---
+++ WBH/Controllers/WBHController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WBH.Controllers
+{
+    public class WBHController : Controller
+    {
+        // GET: WBH
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Sanpham()
+        {
+            return View();
+        }
+        public ActionResult Sale()
+        {
+            return View();
+        }
+        public ActionResult Phukien()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Giohang()
+        {
+            return View();
+        }
+    }
+}
