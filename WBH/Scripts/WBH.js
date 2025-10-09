62d3d855ce9f5c1057e422efa86685810feccc99
Thanh Dang <maithanhdang060106@gmail.com>
2025-10-09 20:48:10 +0700

    update js

--- WBH/Scripts/WBH.js
+++ WBH/Scripts/WBH.js
@@ -2,6 +2,7 @@
     const user = document.getElementById('username').value;
     const pass = document.getElementById('password').value;
 
+    //Thử nghiệm đăng nhập
     // Giả lập tài khoản
     if (user === 'admin' && pass === '123') {
         alert('Đăng nhập thành công!');
@@ -10,4 +11,4 @@
     } else {
         alert('Sai tài khoản hoặc mật khẩu!');
     }
-}
+}   
