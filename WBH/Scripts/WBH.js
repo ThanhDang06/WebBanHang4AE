d0b3b0746b59fe4a7f782ce6060474f40990cd22
Thanh Dang <maithanhdang060106@gmail.com>
2025-10-09 20:43:35 +0700

    update home & login

--- WBH/Scripts/WBH.js
+++ WBH/Scripts/WBH.js
@@ -1,1 +1,13 @@
-﻿
+﻿function login() {
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    // Giả lập tài khoản
+    if (user === 'admin' && pass === '123') {
+        alert('Đăng nhập thành công!');
+        localStorage.setItem('loggedIn', 'true'); // Lưu trạng thái đăng nhập
+        window.location.href = 'sanpham.html';
+    } else {
+        alert('Sai tài khoản hoặc mật khẩu!');
+    }
+}
