afbd29897acbf17a1d78bbf157fcbd27c9ea3d88
Thanh Dang <maithanhdang060106@gmail.com>
2025-11-23 23:57:05 +0700

    add stock

---
+++ WBH/Models/Product.Partial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WBH.Models
+{
+    public partial class Product
+    {
+        public bool IsOutOfStock
+        {
+            get
+            {
+                // Dùng Quantity trong DB để xác định
+                return !this.Quantity.HasValue || this.Quantity.Value <= 0;
+            }
+        }
+    }
+}
