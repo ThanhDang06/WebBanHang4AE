5541eda60f330b190714479bc8948fd8d13d3756
Thanh Dang <maithanhdang060106@gmail.com>
2025-11-25 19:45:45 +0700

    sql

--- WBH/Global.asax.cs
+++ WBH/Global.asax.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -15,7 +16,32 @@ namespace WBH
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            AttachDatabaseIfNotExists();
         }
+        private void AttachDatabaseIfNotExists()
+        {
+            // Đường dẫn tới file .mdf trong App_Data
+            var dbFile = Server.MapPath("~/App_Data/DBFashionStore.mdf");
+            var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Integrated Security=True;";
+
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = $@"
+                        IF NOT EXISTS(SELECT name FROM sys.databases WHERE name = N'DBFashionStore')
+                        BEGIN
+                            CREATE DATABASE DBFashionStore
+                            ON (FILENAME = '{dbFile}')
+                            FOR ATTACH_REBUILD_LOG;
+                        END";
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
         protected void Application_AuthenticateRequest(Object sender, EventArgs e)
         {
             HttpCookie authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
