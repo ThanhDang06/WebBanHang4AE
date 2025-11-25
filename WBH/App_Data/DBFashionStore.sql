e27332ba044cb8d7af4d20e64074d9bbfc206dbb
Thanh Dang <maithanhdang060106@gmail.com>
2025-11-25 18:23:44 +0700

    sql

---
+++ WBH/App_Data/DBFashionStore.sql
@@ -0,0 +1,6 @@
+﻿IF NOT EXISTS(SELECT name FROM sys.databases WHERE name = N'DBFashionStore')
+BEGIN
+    CREATE DATABASE DBFashionStore
+    ON (FILENAME = 'App_Data\DBFashionStore.mdf')
+    FOR ATTACH_REBUILD_LOG;
+END
